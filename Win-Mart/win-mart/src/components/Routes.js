85869a7921667e4784e18579ae2ba8a43081a5a7
dpatel38 <69981731+dpatel38@users.noreply.github.com>
2021-03-23 09:22:45 -0400

    Added home page and contactus page functionality

---
+++ Win-Mart/win-mart/src/components/Routes.js
@@ -0,0 +1,31 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import ContactUs from './ContactUs';
+import Shopping from './Shopping';
+import React, {Component} from 'react';
+import '../index.css';
+import Footer from './Footer';
+import {
+    BrowserRouter as Router, // we are aliasing this module for a cleaner call
+    Route,
+    Link
+    // etc.
+  } from 'react-router-dom';
+
+class Routes extends Component {
+    render(){
+      return(
+        <Router>
+          <div>
+            <Navbar />
+            <hr />
+            <Route name="Home" exact path="/" component={Home}/>
+            <Route name="Shopping" path="/shopping" component={Shopping}/>
+            <Route name="Contact Us" path="/contactus" component={ContactUs} />
+            <Footer />
+          </div>
+        </Router>
+      );
+    }
+  }
+  export default Routes;
