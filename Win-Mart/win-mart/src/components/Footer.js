ed1c77e0f6919fb8526585a3dd4a969f922de543
gsidburycrawford <66580946+gsidburycrawford@users.noreply.github.com>
2021-03-24 11:17:17 -0400

    adjusted navbar to center alignment

--- Win-Mart/win-mart/src/components/Footer.js
+++ Win-Mart/win-mart/src/components/Footer.js
@@ -30,29 +30,8 @@ const Footer = () => {
         <div className="container text-center text-md-left">
           <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
-              <p className="text-uppercase font-weight-bold">Contact Us</p>
-              <p>You can contact us custService_winmart@gmail.com</p>
-              <div className="social-media">
-        <div className="icons-wrapper">
-          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-            <img src="https://icons.iconarchive.com/icons/hopstarter/rounded-square/32/Social-Network-Facebook-icon.png" alt="facebook"></img>
-          </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-            <img src="https://icons.iconarchive.com/icons/limav/flat-gradient-social/32/Instagram-icon.png" alt="Instagram"></img>
-          </a>
-          <a href="https://twitter.com/?lang=en" target="_blank" rel="noopener noreferrer">
-            <img src="https://icons.iconarchive.com/icons/yootheme/social-bookmark/32/social-twitter-box-blue-icon.png" alt="Twitter"></img>
-          </a>
-          <a href="https://www.pinterest.com/?utm_source=google&utm_medium=paid_search&utm_campaign=seo_copy_test&utm_term=pinterest&utm_pai=12551869605_122367574507_506837571519&creative_id=506837571519_kwd-27887497160&gclid=CjwKCAjwxuuCBhATEiwAIIIz0Sj2dKP7SCninWa0a7qdb0KLnJYVVeppv57OReWmdEaCB97domFIBxoCsEgQAvD_BwE&gclid=CjwKCAjwxuuCBhATEiwAIIIz0Sj2dKP7SCninWa0a7qdb0KLnJYVVeppv57OReWmdEaCB97domFIBxoCsEgQAvD_BwE" target="_blank" rel="noopener noreferrer">
-            <img src="https://icons.iconarchive.com/icons/limav/flat-gradient-social/32/Pinterest-icon.png" alt="Pinterest"></img>
-          </a>
-        </div>
-      </div>
-=======
-            <div className="col-md-3 mt-md-0 mt-3 ">
               <p className="text-uppercase font-weight-bold text-center">Contact Us</p>
-              <p className="text-center">Contact us at custService@winmart.com</p>              
->>>>>>> 82dd031225548ca8c730b953707a4eaeed89fe60
+              <p className ="text-center">You can contact us at <br></br>custService_winmart@gmail.com</p>
             </div>
             <div className="col-md-3 mb-md-0 mb-3">
               <p className="text-uppercase font-weight-bold text-center">Return Policy </p>
--- Win-Mart/win-mart/src/components/Navbar.js
+++ Win-Mart/win-mart/src/components/Navbar.js
@@ -5,11 +5,8 @@ import {
     // etc.
   } from 'react-router-dom';
 import React, {Component} from 'react';
-<<<<<<< HEAD
-=======
 import Logo from '../images/logo3.png';
 import '../App.css';
->>>>>>> 82dd031225548ca8c730b953707a4eaeed89fe60
 
 class Navbar extends Component {
     render(){
